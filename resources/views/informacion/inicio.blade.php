d67b6da08e258bf21c048bbe82d6089d93b9b522
PG210 <pedrocuasquer21095@gmail.com>
2021-04-15 23:33:29 -0500

    vista inicial

---
+++ resources/views/informacion/inicio.blade.php
@@ -0,0 +1,37 @@
+<div class="container">
+           <br>
+            <h2 class="alert alert-dark text-center">BIENVENIDOS A COOTRANUR LTDA</h2><br>
+            
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="{{url('img/c5.jpg')}}" class="d-block w-100 " alt="cargando imagen...">
+                </div>
+                <div class="carousel-item">
+                <img src="{{url('img/c2.jpg')}}" class="d-block w-100" alt="cargando imagen ...">
+                </div>
+                <div class="carousel-item">
+                <img src="{{url('img/c3.jpg')}}" class="d-block w-100" alt="cargando imagen...">
+                </div>
+                <div class="carousel-item">
+                <img src="{{url('img/c4.jpg')}}" class="d-block w-100" alt="cargando imagen...">
+                </div>
+                
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            
+            </div>
+     </div>
--- resources/views/informacion/nav.blade.php
+++ resources/views/informacion/nav.blade.php
@@ -1,42 +1,48 @@
-<nav class="navbar navbar-expand-lg navbar-light beta bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="#" class="imagen" alt="cargando ....">
-            </a>
-             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon">
-            </span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{url('/')}}"><h3 class="letra">Inicio</h3></a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#"><h3 class="letra">Historia</h3></a>
-              </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#"><h3 class="letra">Ubicacion</h3></a>
-                </li>
-            </ul>
-            <ul class="nav justify-content-end">
-            @if (Route::has('login'))
-                <li class="nav-item">
-                <div>
-                    @auth
-                        <a class="nav-link" href="{{ url('/dashboard') }}" >Dashboard</a>
-                    @else
-                        <a class="nav-link" href="{{ route('login') }}"><b>Iniciar sesion</b></a>
-                </li>
-                <li class="nav-item">
-                        @if (Route::has('register'))
-                            <a  class="nav-link" href="{{ route('register') }}"><b>Registrarse<b></a>
-                        @endif
-                    @endauth
-                </div>
-                </li>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#3B58F8;">
+  <div class="container-fluid">
+    <button class="navbar-toggler text-dark " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon text-dark">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Menu</span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="#"><b>Inicio</b></a>
+        </li>
+    
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <b>Nosotros</b>
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Mision</a></li>
+            <li><a class="dropdown-item" href="#">Vision</a></li>
+            <li><a class="dropdown-item" href="#">Ubicacion</a></li>
+          </ul>
+        </li>
+        
+      </ul>
+      <div class="d-flex">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        @if (Route::has('login'))
+            <li class="nav-item">
+            @auth
+            <a class="nav-link text-white" href="{{ url('/dashboard') }}">Regresar</a>
+            @else
+            <a class="nav-link text-white "  href="{{ route('login') }}"><b>Iniciar sesion</b></a>
+            </li>
+            <li class="nav-item">
+                @if (Route::has('register'))
+                <a  class="nav-link text-white  " href="{{ route('register') }}"><b>Registrarse<b></a>
                 @endif
-           </ul>
-            </div>
-        </div>
-        </nav>
+            @endauth
+            </li>
+            @endif
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+
+       
