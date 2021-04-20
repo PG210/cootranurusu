efe9ecf29527ff2f26a138e115847b214c7ca4db
Christian Guerra <christianpastopitbull@gmail.com>
2021-04-20 15:06:03 -0500

    logo inicio ventana

--- resources/views/informacion/nav.blade.php
+++ resources/views/informacion/nav.blade.php
@@ -1,7 +1,29 @@
+<br>
+<div class="text-center">
+<img src="{{url('img/2.png')}}" class="img-fluid" alt="Cargando Imagen...">
+</div>
+<br>
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#3B58F8;">
   <div class="container-fluid">
+  <a class="navbar-brand" href="{{url('/')}}">
+                <img src="{{url('img/2.png')}}" class="imagen" alt="cargando imagen...." > 
+               <style>
+                img.imagen{
+                        position: relative;
+                        width: 230px; height: 50px;
+                        top: 2px; /* ajustar los valores para mover la imagen */
+                        left: 0px;
+                        margin: 1%;
+                        border:1px solid #fff;
+                        display: block;
+                      }
+                  .color{
+                      background-color:#3B58F8;
+                  }
+                </style>
+                </a>
     <button class="navbar-toggler text-dark " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon text-dark">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Menu</span>
+      <span class="navbar-toggler-icon text-dark"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
